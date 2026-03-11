226621206948f8aa135e0a130d8ba4d01da593e6
yusufalsaqri <y.alsaqri@wmich.edu>
2026-03-11 16:30:33 -0400

    learning_bash.sh

---
+++ learning_bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+greeting="Hi"
+name="Yusuf"
+echo "$greeting $name"
+echo "This script is helping you learn bash."
+mkdir -p ~/Desktop/Bioinformatics/bash_scripts
+mv ~/Desktop/bash_template.txt ~/Desktop/Bioinformatics/bash_scripts/bash_template.sh
